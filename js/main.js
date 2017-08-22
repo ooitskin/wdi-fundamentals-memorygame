aa2c6aff17705b1886e841ec9435189e34bf3fd3
Kin L Lim <Lim.Kin@gmail.com>
2017-08-21 20:03:35 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log('User flipped ' + cardsInPlay);
-
-if(cardsInPlay.length === 2){
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert('You found a match!');
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log('You found a match!');
 	} else {
-		alert('Sorry, try again.')
+		console.log('Sorry, try again')
 	}
 }
+
+function flipCard(cardId){
+	if(cardsInPlay.length === 2){
+		checkForMatch();
+	};
+	console.log('User flipped ' + cards[cardId]);
+};
+
+flipCard([0]);
+flipCard([2]);
+

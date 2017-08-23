5e05216863878ceecc7cc58a8b564ed1d2f1e02c
Kin L Lim <Lim.Kin@gmail.com>
2017-08-22 21:29:28 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -31,18 +31,33 @@ var checkForMatch = function() {
 	}
 };
 
-var flipCard = function(cardId){
+var flipCard = function(){
+	var cardId = this.getAttribute("data-id");
+	var card = cards[cardId];
+	this.setAttribute("src", card.cardImage);
+	this.setAttribute("class", "clicked");
+
 	if(cardsInPlay.length === 2){
 		checkForMatch();
 	}
 	console.log('User flipped ' + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].cardImage);	
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
 };
 
-flipCard([0]);
-flipCard([2]);
+var createBoard = function(){
+	for(var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		var board = document.getElementById("game-board");
+		board.appendChild(cardElement);
+	}
+};
+
+createBoard();
 
 
 
